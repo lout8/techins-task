2b057ceef0fabf6aae8b145e80d34ab59635216d
Elissaios <e.loutos8@gmail.com>
2023-12-07 12:43:32 +0200

    fix roots

---
+++ components/layout/LayoutComponent.js
@@ -0,0 +1,24 @@
+"use client"
+import { useState } from 'react'
+import Providers from "../../lib/providers"
+import Header from "./Header"
+import Footer from './Footer'
+import ScrollToTop from './ScrollToTop'
+
+const LayoutComponent = ({children}) => {
+    const [toggle, setBar] = useState(false)
+    return (
+        <html className=' font-mono' lang="en">
+            <body className={`dark:bg-neutral-800 dark:text-neutral-100 dark:bg-dark-bg-image dark:border-neutral-100 bg-neutral-100 text-neutral-800 bg-fixed bg-bg-image w-full h-screen bg-cover ${toggle === true ? 'overflow-hidden' : 'overflow-scroll'}`}>
+                <Providers>
+                    <ScrollToTop/>
+                    <Header toggle={toggle} setBar={setBar}/>
+                    {children}
+                    <Footer/>
+                </Providers>
+            </body>
+        </html>
+    )
+}
+
+export default LayoutComponent
