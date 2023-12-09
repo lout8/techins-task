b2c76c65b5ee414d17b259c42288313de9c1603e
Elissaios <e.loutos8@gmail.com>
2023-12-09 22:50:02 +0200

    form

--- components/parts/ContactForm.js
+++ components/parts/ContactForm.js
@@ -66,14 +66,14 @@ const ContactForm = () => {
     }
         
     return (
-        <form name='contact' className='flex flex-col gap-5 pr-[40%] max-xl:pr-[30%] max-lg:pr-[20%] max-md:pr-[10%] max-sm:pr-0' method="POST"  onSubmit={e => handleSubmit(e)} action='#' data-netlify="true" netlify-honeypot="bot-field" data-netlify-recaptcha="true">
+        <form name='contact' className='flex flex-col gap-5 pr-[40%] max-xl:pr-[30%] max-lg:pr-[20%] max-md:pr-[10%] max-sm:pr-0' method="POST"  onSubmit={e => handleSubmit(e)} action='#' data-netlify="true" netlify-honeypot="bot-field">
             <p start={start} sent={sent} className={`${start === true ? 'flex overflow-visible' : 'hidden'} ${sent === true ? 'text-green-500' : 'text-red-500'}`}>{statusText}</p>
             <input type="hidden" name="form-name" value="contact"/>
             <input style={{display: "none"}} name="bot-field" />
             <input type="text" name="name" id="name" placeholder="Name" value={formData.name} onChange={e => handleChange(e)} required/>
             <input type="email" name="email" id="email" placeholder="Email" value={formData.email} onChange={e => handleChange(e)} required/>
             <textarea name="message" rows="5" placeholder="Message" value={formData.message} onChange={e => handleChange(e)} required/>
-            <div data-netlify-recaptcha="true"></div>
+            {/* <div data-netlify-recaptcha="true"></div> */}
             <button className='bg-neutral-800 dark:bg-neutral-100 text-neutral-100 dark:text-neutral-800 w-fit text-sm py-4 px-3 rounded-md hover:bg-purple-400 dark:hover:bg-purple-400 transition duration-500' type='submit'>Send Message</button>
         </form>
     )
